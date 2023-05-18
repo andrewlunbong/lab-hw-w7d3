ea13528c5f124cb262d1608f508f594cedd0ebe3
andrewlunbong <andrewlunbong@gmail.com>
2023-05-18 09:44:33 +0100

    done

---
+++ src/Components/FilmList.js
@@ -0,0 +1,22 @@
+import Film from "./Film"
+
+const FilmList = ({films}) =>{
+
+    const filmComponents = films.map( film =>{
+        return (
+            
+           <Film key={film.id} name={film.name} url= {film.url} />
+           
+        )
+    })
+
+    return(
+        <ul>
+            {filmComponents}
+        </ul>
+    )
+
+
+}
+
+export default FilmList
